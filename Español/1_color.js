a49463fef60aa256d0460c2788bd0cca225b0298
EmilioOcelotl <emilio.ocelotl@gmail.com>
2022-03-22 22:28:21 -0600

    aclaraciones en fuentes

--- Español/1_color.js
+++ Español/1_color.js
@@ -1,20 +1,24 @@
-///////////////////////
-/// TRANSFORMADORES ///
-///////////////////////
+/////////////
+/// COLOR ///
+/////////////
 
-// Son funciones que puedes aplicarse a las sources para transformarlas de diferentes maneras
+// Son funciones de color que se pueden aplicar a las fuentes para transformarlas de diferentes maneras
 
-//Los transformadores se escriben normalmente entre la source y la salida
+// Las funciones de color en Hydra son: brightness() contrast() color() colorama() posterize() shift() invert() luma() thresh() saturate() hue() sum() r() g() b() a()
+
+//Los transformadores se escriben normalmente entre la fuente y la salida
+
+// Un caso general con transformaciones de color tiene la forma: fuentes(parámetros).transformador(parámetros).out() 
+
 //Ejemplo:
 osc().brightness().out() // Esto corre un oscilador con un cambio en el brillo, todos los parámetros usados son los que están por defecto.
 //Se corre presionando shift+Enter
 
-//Otra manera de escribir la misma sintaxis es la siguiente:
+//Otra manera de escribir la misma sintaxis (indentación) es la siguiente :
 osc()
   .brightness()
 .out()
-//Se puede correr bloques de cógido (desarrolado en líneas separadas) presionando ctrl+Enter
-
+//Se puede correr bloques de código (desarrolado en líneas separadas) presionando ctrl+Enter
 
 /////////
 // Brillo
@@ -23,11 +27,11 @@ osc()
 osc().brightness().out()
 //Brillo con parámetro por defecto: (0.4) = negro
 
-//Brightness incrementa los niveles de luz en la imágen, se puede aplicar a cualquier source
+//Brightness incrementa los niveles de luz en la imagen, se puede aplicar a cualquier source
 //Los números negativos resultan en texturas oscuras
 //Números mayores a 0 resultan adición de brillo
 
-//Ejemlplos
+//Ejemplos
 voronoi().brightness().out() // Voronoi con brillo por defecto
 voronoi().brightness(-0.5).out() // Vornoi con brillo en negativo, resultando en texturas más oscuras
 voronoi().brightness(0.8).out() // Vonoroi con brillo saturado, el número máximo antes de la saturación total del brillo depende de las texturas de color que se generen
@@ -58,3 +62,63 @@ osc().color().out()
 
 osc().colorama().out()
 //Brillo con parámetros por defecto: (0.4) = negro
+
+/////////
+// Posterize 
+//Sintaxis: 
+//
+
+/////////
+// Shift
+//Sintaxis: 
+//
+
+/////////
+// Invert 
+//Sintaxis: 
+//
+
+/////////
+// Lumma 
+//Sintaxis: 
+//
+
+/////////
+// Thresh  
+//Sintaxis: 
+//
+
+/////////
+// Saturate 
+//Sintaxis: 
+//
+
+/////////
+// Hue 
+//Sintaxis: 
+//
+
+/////////
+// Sum  
+//Sintaxis: 
+//
+
+/////////
+// R 
+//Sintaxis: 
+//
+
+/////////
+// G
+//Sintaxis: 
+//
+
+/////////
+// B 
+//Sintaxis: 
+//
+
+/////////
+// A 
+//Sintaxis: 
+//
